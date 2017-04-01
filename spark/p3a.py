15690231049f1ec6d7437c04f8adc32f1c33af54
CarlosTheran <carlos.theran@upr.edu>
2017-04-01 18:56:25 -0400

    Update p3a.py

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,6 @@
 #Code here
+import csv
+data_student = sc.textFile("/home/carlos/Files/studentsPR.csv")
+Var_student=data_student.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[0])
+sol1=Var_student.filter(lambda x: x[5] == 'F').filter(lambda x: x[2] == '71381')
+sol1.foreach(print)
